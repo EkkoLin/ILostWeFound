11103e0e11ecf4e0d371ec803a12da7e78be9516
Ekko Lin <sihansolotop@gmail.com>
2017-11-11 23:42:29 -0500

    LostView updated

--- LostView.js
+++ LostView.js
@@ -1,16 +1,61 @@
-import React, { Component } from "react";
-import { View, Button, Text } from "react-native";
+import React, { PureComponent, Component } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { List, ListItem } from "react-native-elements";
 import { StackNavigator } from "react-navigation";
 
-export default class LostView extends Component {
+export default class LostView extends PureComponent {
   static navigationOptions = {
     title: "LostView"
   };
+
+  //   state = {selected: (new Map(): Map<string, boolean>)};
+
+  _keyExtractor = (item, index) => item.id;
+
+  onPressItem = id => {
+    // copy the map rather than modifying state.
+    const selected = new Map(state.selected);
+    selected.set(id, !selected.get(id)); // toggle
+    return { selected };
+  };
+
+  renderItem = ({ item }) => (
+    <LostItem
+      id={item.id}
+      onPressItem={this.onPressItem}
+      selected={!!this.state.selected.get(item.id)}
+      title={item.title}
+    />
+  );
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Lost Screen</Text>
-      </View>
+      <List>
+        <FlatList
+          data={losts}
+          extraData={this.state}
+          renderItem={({ item }) => 
+            <ListItem 
+                title={item.title} 
+                subtitle={item.key}
+            />}
+        />
+      </List>
     );
   }
 }
+
+const losts = [
+  {
+    title: "Title text",
+    key: "item1"
+  },
+  {
+    title: "Title text2",
+    key: "item2"
+  },
+  {
+    title: "Title text3",
+    key: "item3"
+  }
+];

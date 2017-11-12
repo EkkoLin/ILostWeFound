42db437455f8294e823e07dc6b42a176c1f7d639
Ekko Lin <sihansolotop@gmail.com>
2017-11-12 02:15:10 -0500

    Form view updated

--- Form.js
+++ Form.js
@@ -1,51 +1,78 @@
-import React, { Component } from 'react';
-import { Alert, AppRegistry, Text, TextInput, View, Button } from 'react-native';
+import React, { Component } from "react";
+import { Alert, AppRegistry, Text, TextInput, View } from "react-native";
+import {
+  FormLabel,
+  FormInput,
+  FormValidationMessage,
+  Button
+} from "react-native-elements";
 import { StackNavigator } from "react-navigation";
 
 export default class Form extends Component {
-    static navigationOptions = {
-        title: "Post New Object"
+  static navigationOptions = {
+    title: "Post New Object"
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      url: "",
+      where: "",
+      contact: "",
+      reward: ""
     };
 
-    constructor() {
-        super();
-        this.state = {
-            name: '',
-            url: '',
-            where: '',
-            contact: '',
-            reward: '',
-        };
+    this.onButtonPress = this.onButtonPress.bind(this);
+  }
 
-        this.onButtonPress = this.onButtonPress.bind(this);
-    }
+  onButtonPress() {
+    let alertString = "Your name is " + this.state.name;
+    Alert.alert(alertString);
+  }
 
-    onButtonPress() {
-        let alertString = 'Your name is ' + this.state.name;
-        Alert.alert(alertString);
-    }
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <FormLabel>NAME</FormLabel>
+        <FormInput
+          onChangeText={name =>
+            typeof name !== 'string' ? (
+              <FormValidationMessage>
+                {"This field is required"}
+              </FormValidationMessage>
+            ) : (
+              this.setState({ name })
+            )}
+        />
 
-    render() {
-        const { navigate } = this.props.navigation;
-        return (
-            <View>
-                <Text>Name: </Text>
-                <TextInput onChangeText={ (name) => this.setState({name:name}) } />
-                <Text>imageURL: </Text>
-                <TextInput onChangeText={ (url) => this.setState({url}) } />
-                <Text>Where: </Text>
-                <TextInput onChangeText={ (where) => this.setState({where}) } />
-                <Text>Contact: </Text>
-                <TextInput onChangeText={ (contact) => this.setState({contact}) } />
-                <Text>Reward: </Text>
-                <TextInput onChangeText={ (reward) => this.setState({reward}) } />
-                <Button
-                    title="Submit"
-                    color="#841584"
-                    onPress={ this.onButtonPress }
-                    accessibilityLabel="Submit"
-                />
-            </View>
-        );
-    }
+        <FormLabel>WHERE</FormLabel>
+        <FormInput onChangeText={where => this.setState({ where })} />
+        <FormValidationMessage>
+          {"This field is required"}
+        </FormValidationMessage>
+
+        <FormLabel>Contact</FormLabel>
+        <FormInput onChangeText={contact => this.setState({ contact })} />
+        <FormValidationMessage>
+          {"This field is required"}
+        </FormValidationMessage>
+
+        <FormLabel>REWARD</FormLabel>
+        <FormInput onChangeText={reward => this.setState({ reward })} />
+        <FormValidationMessage>
+          {"This field is required"}
+        </FormValidationMessage>
+
+        <Button
+          large
+          title="Submit"
+          backgroundColor="rgb(235, 192, 47)"
+          onPress={this.onButtonPress}
+          accessibilityLabel="Submit"
+        />
+      </View>
+    );
+  }
 }
